bc0665f4a7757b7e745d643c3c8a2ec411d30fc1
Liam Hurt <liamhurt51@gmail.com>
2018-03-28 06:43:30 -0700

    Adding a maze running bot that attempts to run a maze as far as possible... values are turned way down because the bots were escaping the maze

---
+++ Assets/Scripts/MazeWalker/MazeBrain.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MazeBrain : MonoBehaviour {
+
+	public float dstTravelled = 0.0f;
+	public GeneticAlgo GA;
+	public GameObject eyes;
+
+	int geneLength = 2;
+	Vector3 startPos;
+	bool seeWall = true;
+	bool alive = true;
+
+	public void Init() {
+		// Initialize the genes
+		// 0 = Forward
+		// 1 = Angled Turn
+		GA = new GeneticAlgo(geneLength, 360);
+		startPos = this.transform.position;
+	}
+
+	void OnCollisionEnter(Collision col) {
+		if (col.gameObject.tag == "dead") {
+			dstTravelled = 0.0f;
+			alive = false;
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (!alive) return;
+
+		seeWall = false;
+		RaycastHit hit;
+		Debug.DrawRay (eyes.transform.position, eyes.transform.forward * 0.5f, Color.red);
+		if (Physics.SphereCast (eyes.transform.position, 0.1f, eyes.transform.forward, out hit, 0.5f)) {
+			if (hit.collider.gameObject.tag == "wall") seeWall = true;
+		}
+	}
+
+	void FixedUpdate() {
+		if (!alive) return;
+
+		// read genes
+		float h = 0.0f;
+		float v = GA.GetGene (0);
+
+		if (seeWall) h = GA.GetGene (1);
+
+		this.transform.Translate (0.0f, 0.0f, v * 0.0002f);
+		this.transform.Rotate (0.0f, h, 0.0f);
+		dstTravelled = Vector3.Distance (startPos, this.transform.position);
+	}
+}
---
+++ Assets/Scripts/MazeWalker/MazePopManager.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+
+public class MazePopManager : PopManager {
+
+	// Use this for initialization
+	protected override void Start () {
+		for (int i = 0; i < popSize; i++) {
+			GameObject bot = Instantiate (botPrefab, this.transform.position, this.transform.rotation);
+
+			bot.GetComponent<MazeBrain> ().Init ();
+			population.Add (bot);
+		}
+	}
+
+	protected override GameObject Breed (GameObject P1, GameObject P2)
+	{
+		GameObject offspring = Instantiate (botPrefab, this.transform.position, this.transform.rotation);
+		MazeBrain brain = offspring.GetComponent<MazeBrain> ();
+
+		if (Random.Range (0.0f, 100.0f) == 1.0f) {
+			brain.Init ();
+			brain.GA.Mutate ();
+		} else {
+			brain.Init ();
+			brain.GA.Combine (P1.GetComponent<MazeBrain> ().GA, P2.GetComponent<MazeBrain> ().GA);
+		}
+
+		return offspring;
+	}
+
+	protected override void BreedNewPop ()
+	{
+		List<GameObject> sortedList = population.OrderBy(o => o.GetComponent<MazeBrain>().dstTravelled).ToList();
+
+		population.Clear ();
+		// Breed upper half of sorted list
+		for (int i = (int)(sortedList.Count / 2.0f) - 1; i < sortedList.Count - 1; i++) {
+			population.Add (Breed (sortedList [i], sortedList [i + 1]));
+			population.Add (Breed (sortedList [i + 1], sortedList [i]));
+		}
+
+		// Destory all previous population
+		for (int i = 0; i < sortedList.Count; i++) {
+			Destroy (sortedList [i]);
+		}
+
+		generation++;
+	}
+}
